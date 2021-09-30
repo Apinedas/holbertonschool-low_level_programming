a14201ae7560fe6291ebd6e57c0a8a01b2ea48c6
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-09-30 21:57:22 +0000

    Fifth script

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
+/**
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
