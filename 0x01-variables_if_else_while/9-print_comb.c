b65ed02ceb4f475ab88f775d399066545ba486e3
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-02 01:22:21 +0000

    Scripts for second project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+/**
+ * main - Prints hex numbers
+ * Returns: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+
+	c = '0';
+	while (c <= '8')
+	{
+		putchar(c);
+		putchar(',');
+		c++;
+	}
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
