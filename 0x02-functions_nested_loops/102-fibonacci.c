b543d4d6a90889d60194a876826365fc70e76af0
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-06 00:44:02 +0000

    Added Fibonacci exercises

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line.
+ * Return: Always 0;
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int pos;
+	long int num1, num2, fib;
+
+	num1 = 1;
+	num2 = 2;
+	printf("%ld\n%ld\n", num1, num2);
+	for (pos = 1; pos <= 48; pos++)
+	{
+		fib = num1 + num2;
+		num1 = num2;
+		num2 = fib;
+		printf("%ld\n", fib);
+	}
+	return (0);
+}
