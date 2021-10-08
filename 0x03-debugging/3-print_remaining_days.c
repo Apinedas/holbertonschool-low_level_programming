95d0c21e5ed4be79d54659522a12aae9e3e64276
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-08 14:44:26 +0000

    Corrections on days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,8 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month >= 2 && day >= 60 && year != 0)
 		{
-			printf("Day of the year: %d\n", day);
+			day++;
+			printf("Day of the year: %d\n", (day - 1);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 		else if (year == 0)
