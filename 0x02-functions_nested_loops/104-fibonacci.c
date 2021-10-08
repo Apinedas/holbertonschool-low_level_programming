5b063b67d0175139b3cb08a28aa81316a01718a0
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-08 23:46:22 +0000

    Correction on 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,16 +13,16 @@ int main(void)
 
 	num1 = 1;
 	num2 = 2;
-	printf("%.0lf, %.0lf, ", num1, num2);
+	printf("%.0f, %.0f, ", num1, num2);
 	for (pos = 1; pos <= 96; pos++)
 	{
 		fib = num1 + num2;
 		num1 = num2;
 		num2 = fib;
 		if (pos != 96)
-			printf("%.0lf, ", fib);
+			printf("%.0f, ", fib);
 		else
-			printf("%.0lf\n", fib);
+			printf("%.0f\n", fib);
 	}
 	return (0);
 }
