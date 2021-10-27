54c7b0b307e20c661dd61e78b3727a0984c299bd
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-27 00:15:05 +0000

    Correction on 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,11 +18,15 @@ char *str_concat(char *s1, char *s2)
 
 	len1 = 0;
 	len2 = 0;
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
 	for (i = 0; s1[i] != '\0'; i++)
 		len1 += 1;
 	for (i = 0; s2[i] != '\0'; i++)
 		len2 += 1;
-	ret = malloc(len1 + len2);
+	ret = malloc(len1 + len2 + 1);
 	if (ret == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
