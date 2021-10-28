986f16483e7e2d2769e21951ff81a53c1fc7981b
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-28 04:17:14 +0000

    Correction on 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -28,7 +28,7 @@ char **strtow(char *str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0' || str[0] != ' ')
 		{
 			for (j = i + 1; str[j] != ' '; j++)
 				wlen += 1;
