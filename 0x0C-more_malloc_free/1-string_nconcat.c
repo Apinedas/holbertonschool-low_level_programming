bf56da148aa42f2bc337f578a0b8fdfba5cae683
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-10-29 03:36:43 +0000

    Correction on 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,12 +19,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = len2 = 0;
 	if (s1 == NULL)
 	{
-		s1 = malloc(1);
+		s1 = malloc(sizeof(char));
 		*s1 = '\0';
 	}
 	if (s2 == NULL)
 	{
-		s2 = malloc(1);
+		s2 = malloc(sizeof(char));
 		s2 = '\0';
 	}
 	for (i = 0; s1[i] != '\0'; i++)
