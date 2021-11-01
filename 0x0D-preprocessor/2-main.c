5cbf59bbe3684fda617b313fbd7aa8fb73e51f3b
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-01 17:37:32 +0000

    Betty style correction on 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
- /**
-  * main - prints the name of the file it was compiled from, followed by a new line.}
-  * Return: Always 0
-  */
+/**
+ * main - prints name of the file it was compiled from
+ * Return: Alwaus 0
+ */
 
 int main(void)
 {
