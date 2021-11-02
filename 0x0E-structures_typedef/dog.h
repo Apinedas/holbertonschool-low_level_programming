c96e12906d4cb63cfbdf2c6e01b6ab05e0c5a19e
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-02 02:06:53 +0000

    Betty correction on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,9 +21,12 @@ struct dog
 typedef struct dog dog_t;
 
 /**
- * Prototypes
+ * Prototypes - Prototypes for functions
+ * @d: pointer to dog info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner name
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
