e8eb6333a23997312459339e52f1ff5e7533d9c6
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-16 14:56:19 +0000

    General corrections

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,11 +11,12 @@ size_t listint_len(const listint_t *h)
 	unsigned int nodes;
 
 	nodes = 0;
-	while (h->next != NULL)
+	while (h && h->next != NULL)
 	{
 		nodes += 1;
 		h = h->next;
 	}
-	nodes += 1;
+	if (h)
+		nodes += 1;
 	return (nodes);
 }
