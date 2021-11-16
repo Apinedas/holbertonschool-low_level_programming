747df10ab84ffc9a59f733a91089b7a0d34fb988
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-16 15:15:20 +0000

    General corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *aux, *ret;
 
 	position = 0;
-	if (head)
+	if (head && (*head))
 		aux = *head;
 	else
 		aux = NULL;
