c73b54d2cba40213b74006be19d6bd8df2ce2ad7
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-17 22:08:52 +0000

    Added 103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- * distance_nodes2 - Calcs the distance between two nodes
- * @n1: Pointer to node 1
- * @n2: Pointer to node 2
- * Return: Distance from node 1 to node 2
- */
-
-unsigned int distance_nodes2(listint_t *n1, listint_t *n2)
-{
-        unsigned int distance;
-        const listint_t *aux;
-
-        distance = 0;
-        aux = n1;
-        while (aux && aux != n2)
-        {
-                distance += 1;
-                aux = aux->next;
-        }
-        return (distance);
-}
-
-/**
  * find_listint_loop - finds the loop in a linked list.
  * @head: List's head pointer
  * Return: The address of the node where the loop starts,
@@ -31,17 +9,22 @@ unsigned int distance_nodes2(listint_t *n1, listint_t *n2)
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	unsigned int position;
-	listint_t *aux;
+	listint_t *fast, *slow;
 
-	aux = head;
-	position = 0;
-	while (aux && aux->next)
+	fast = head;
+	slow = head;
+	while ((fast && slow && fast != slow) || (fast == slow && fast == head))
 	{
-		if (position > distance_nodes2(head, aux))
-			return (aux);
-		aux = aux->next;
-		position += 1;
+		if (fast == NULL || slow == NULL)
+			return (NULL);
+		fast = (fast->next)->next;
+		slow = slow->next;
 	}
-	return (NULL);
+	slow = head;
+	while (fast && slow && fast != slow)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (fast);
 }
