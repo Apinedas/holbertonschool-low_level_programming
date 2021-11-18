9b39dc98bd49aa4e85b05d86d601d27f93875fe1
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2021-11-18 19:04:41 +0000

    New way on 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: Pointer to string to measure
- * Return: Lenght of the string
- */
-
-int _strlen(const char *s)
-{
-	int i;
-	int len;
-
-	i = 0;
-	len = 0;
-	while (*(s + i) != '\0')
-	{
-		len = len + 1;
-		i++;
-	}
-	return (len);
-}
-
-/**
- * twopow - calcs 2 raised to the n'th power
- * @n: Desired power
- * Return: Result
- */
-
-int twopow(int n)
-{
-	int ret;
-
-	if (n == 0)
-		return (1);
-	ret = 2 * twopow(n - 1);
-	return (ret);
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: Pointer to a string which contains the binary number
  * Return: Converted number or 0 if the string is NULL or if there is
@@ -46,21 +9,24 @@ int twopow(int n)
 
 unsigned int binary_to_uint(const char *b)
 {
-	int len, power;
 	unsigned int ret;
+	int i, j;
 
-	len = (_strlen(b) - 1);
 	ret = 0;
-	power = 0;
+	i = 0;
+	j = 0;
 	if (b == NULL)
 		return (0);
-	while (len >= 0)
+	while (b[i] != '\0')
+		i++;
+	i--;
+	while (i >= 0)
 	{
-		if (b[len] == '0' || b[len] == '1')
+		if (b[i] == '0' || b[i] == '1')
 		{
-			ret += (b[len] - '0') * twopow(power);
-			len--;
-			power++;
+			ret += (b[i] - '0') << j;
+			i--;
+			j++;
 		}
 		else
 			return (0);
