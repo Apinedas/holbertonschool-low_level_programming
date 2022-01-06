906a6b60d79bd30e753127dfffabff8ed9fd1689
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-01-06 21:11:26 +0000

    Added C files

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * 4-free_dlistint.c - frees a dlistint_t list.
+ * @head: pointer to list's head
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *curr;
+
+    while (head)
+    {
+        curr = head->next;
+        free(head);
+        head = curr;
+    }
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: Pointer to pointer of list's head
+ * @idx: Index to insert new node
+ * @n: int to insert
+ * Return:
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *curr, *new;
+	unsigned int actual_idx = 0;
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return NULL;
+	new->n = n;
+	curr = *h;
+	while (curr)
+	{
+		if (actual_idx == idx)
+		{
+			new->prev = curr;
+			new->next = curr->next;
+			curr->next = new;
+			return (new);
+		}
+		else
+		{
+			curr = curr->next;
+			actual_idx++;
+		}
+	}
+	return (NULL);
+}
